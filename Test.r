e985d8ea3d6c58194930a7a6622f09b43a904899
Eduardo Lozas <128528098+LaloLozas16041@users.noreply.github.com>
2023-10-06 11:08:37 -0700

    Upgrade
    NA

--- Test.r
+++ Test.r
@@ -1,20 +1,6 @@
 install.packages("rmarkdown")
-install.packages("pandoc")
+install.packages("knitr")
 
 library(rmarkdown)
-render("1-example.Rmd")
-
-?rmarkdown::pandoc_available
-
-pandoc_available(version = NULL, error = FALSE)
-pandoc_version()
-
-
-## Not run: 
-library(rmarkdown)
-if (pandoc_available())
-  cat("pandoc", as.character(pandoc_version()), "is available!\n")
-if (pandoc_available("1.12.3"))
-  cat("required version of pandoc is available!\n")
-## End(Not run)
+library(knitr)
 
